e4998aea0f7561a6791f8823a3168f5157118b23
McAlvaro <mc.alv641@gmail.com>
2023-09-02 01:34:03 -0400

    Minor Fixes

--- lua/startup/themes/dashboard.lua
+++ lua/startup/themes/dashboard.lua
@@ -56,7 +56,7 @@ local settings = {
         title = "Footer",
         margin = 5,
         content = { "by Mc Alvaro" },
-        highlight = "String",
+        highlight = "Number",
         default_color = "",
         oldfiles_amount = 0,
     },
