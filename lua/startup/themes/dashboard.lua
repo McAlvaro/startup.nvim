8066322c4f6faf1dbb9e916759c227e83039b84d
McAlvaro <mc.alv641@gmail.com>
2023-09-02 01:37:34 -0400

    Minor Fixes

--- lua/startup/themes/dashboard.lua
+++ lua/startup/themes/dashboard.lua
@@ -66,7 +66,7 @@ local settings = {
         cursor_column = 0.5,
         empty_lines_between_mappings = true,
         disable_statuslines = true,
-        paddings = { 1, 3, 8, 0 },
+        paddings = { 1, 3, 15, 0 },
     },
     mappings = {
         execute_command = "<CR>",
